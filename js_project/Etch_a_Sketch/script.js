106e0dbb044ced7d60efdcfe7e806612e89b0cae
Dan01011997 <f2016915@pilani.bits-pilani.ac.in>
2024-12-15 01:33:52 +0530

    functional button to clear the sketchboard added

--- js_project/Etch_a_Sketch/script.js
+++ js_project/Etch_a_Sketch/script.js
@@ -3,25 +3,33 @@ const buttonPannel=document.querySelector(".buttonpannel");
 const buttonGrid = document.createElement("button");
 const buttonEraser=document.createElement("button");
 const buttonMarker=document.createElement("button");
+const buttonClear=document.createElement("button");
 
 buttonPannel.style.height=`${window.innerHeight}px`;
 buttonPannel.style.flex=1;
 buttonGrid.textContent="GRID";
 buttonGrid.style.fontSize="60px";
 buttonPannel.appendChild(buttonGrid);
+
 buttonEraser.textContent="ERASER";
-buttonEraser.style.fontSize="30px";
-buttonPannel.appendChild(buttonEraser);
-buttonMarker.textContent="MARKER";
-buttonMarker.style.fontSize="30px";
+buttonEraser.style.fontSize="25px";
 buttonPannel.appendChild(buttonMarker);
 
+buttonPannel.appendChild(buttonEraser);
+
+buttonMarker.textContent="MARKER";
+buttonMarker.style.fontSize="25px";
+
+buttonClear.textContent="CLEAR";
+buttonClear.style.fontSize="25px";
+buttonPannel.appendChild(buttonClear);
+
+let penMode;
 let flag=1;
 if(flag==1){
     makeGrid(16);
     makeSketch();
     flag++;
-console.log("hey")
 }
 buttonGrid.addEventListener("click",takeGridNumber)
 function takeGridNumber(){
@@ -30,11 +38,12 @@ function takeGridNumber(){
 
     if(gridValue && gridValue<=100){
         flag++;
-        flagEraser=0;
-        flagMarker=0;
+       penMode=null;
        removeprevGrid();
     makeGrid(gridValue);
     makeSketch();
+    buttonPannel.firstChild.style.background="beige";
+    buttonPannel.firstChild.nextSibling.style.background="beige";
    
 }
 else
@@ -46,7 +55,7 @@ else
 
 function makeGrid(value){
     const computedWidth = window.getComputedStyle(container).width;
-
+    console.log("hi")
     for(let i=0;i<value*value;i++){
 
                 const div=document.createElement("div");
@@ -57,17 +66,17 @@ function makeGrid(value){
                 div.style.height=`${window.innerHeight/value}px`
                 container.appendChild(div);
             }
+            choosePen(value);
         
 
     }
     function makeSketch(){
         Array.from(container.children).forEach((div) => 
             {
-                console.log("yo")
+               
     
            // div.addEventListener("mouseleave",resetDiv);
            div.addEventListener("mousedown",(e)=>{
-            console.log("bdas")
             container.addEventListener("mousemove",hover);
 
            });
@@ -79,62 +88,77 @@ function makeGrid(value){
 
     function hover(e){ 
         e.preventDefault();
-        console.log(e.buttons)
         if (e.buttons == 0) {
             container.removeEventListener("mousemove", hover);
           } 
            e.target.style.backgroundColor=getPen();
-           console.log(`flageraser; ${flagEraser} flag marker: ${flagMarker}`)
+           if(!getPen() || getPen()==="beige")
+           {
+            e.target.style.opacity=1;
+            return;
+           }
+          // console.log(`flageraser; ${flagEraser} flag marker: ${flagMarker}`)
            //`rgb(${Math.random()*(e.clientX+e.clientY)},${Math.random()*(e.clientX+e.clientY)},${Math.random()*(e.clientX+e.clientY)}`;
            if(e.target.style.opacity)
             e.target.style.opacity=`${Math.min(parseFloat(e.target.style.opacity)+0.1*parseFloat(e.target.style.opacity),1)}`;
           else
           e.target.style.opacity=0.55;
-        console.log(e.target.style.opacity);
+       
         } 
         
-    function resetDiv(e){
-            e.target.style.backgroundColor="beige";
-    
-           }
 
 function removeprevGrid(){
-    if(container.firstChild){
+    while(container.firstChild){
         container.firstChild.remove();
-removeprevGrid();
+
     }
     return;
 }
-let flagMarker=0;
-let flagEraser=0;
-function choosePen(){
-    buttonMarker.addEventListener("click",()=>{
-        flagMarker=1;
-        flagEraser=0;
 
-        getPen();
+function choosePen(value){
+    buttonMarker.addEventListener("click",()=>{
+    
+penMode="marker"
+        getPen(value);
     });
     buttonEraser.addEventListener("click",()=>{
-        flagEraser=1;
-        flagMarker=0;
-
-        getPen();
-        console.log('hey')
+    
+penMode="eraser"
+        getPen(value);
     });
+    buttonClear.addEventListener("click",()=>{
+penMode="clear";
+getPen(value)
+    })
 }
-function getPen(){
-if(flagMarker===1){
+
+function getPen(value){
+if(penMode==="marker"){
 buttonMarker.style.background="gray";
 buttonEraser.style.background="beige";
+buttonClear.style.background="beige";
+
     return "black";
     
 }
-if(flagEraser===1)
+else if(penMode==="eraser")
 {
     buttonEraser.style.background="gray";
     buttonMarker.style.background="beige";
+    buttonClear.style.background="beige";
+
 
 
     return "beige";}
+if(penMode==="clear"){
+    buttonClear.style.background="gray";
+    buttonMarker.style.background="beige";
+    buttonEraser.style.background="beige";
+    console.log(value)
+    removeprevGrid();
+    makeGrid(value)
+return makeSketch();
 }
-choosePen();
+}
+
+
